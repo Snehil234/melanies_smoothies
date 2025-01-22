f2e0dfcdb4c3922c166f117490d6f326bfb03ecf
Snehil234 <120574592+Snehil234@users.noreply.github.com>
2025-01-22 19:19:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,5 @@ if ingredients_list:
 
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get(" https://www.fruityvice.com/#3/api/fruit/watermelon")
 st.text(smoothiefroot_response)
